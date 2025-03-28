aabe5ecd2bc12ed4832a9c082fbca21dbe7bc8de
muhidtech <muhideen894@gmail.com>
2025-03-28 17:22:56 +0000

    MuhidTech

---
+++ next-sitemap.config.ts
@@ -0,0 +1,9 @@
+import { IConfig } from "next-sitemap";
+
+const config: IConfig = {
+  siteUrl: "https://muhidtech.vercel.app/", // Change this if you get a custom domain
+  generateRobotsTxt: true, // Generates a robots.txt file
+  sitemapSize: 5000,
+};
+
+export default config;
