18b83d790679bdcaba4435fca37c2dc5b4766e0f
muhidtech <muhideen894@gmail.com>
2025-03-28 17:40:02 +0000

    MuhidTech

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,5 @@ module.exports = {
   siteUrl: "https://muhidtech.vercel.app/", // Change this if you get a custom domain
   generateRobotsTxt: true, // Generates a robots.txt file
   sitemapSize: 5000,
+  generateIndexSitemap: false, // Ensures a valid single sitemap.xml
 };
