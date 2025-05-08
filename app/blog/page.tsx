c1126a6cbc4bd4dbb1efef1359f5ecaadf14110c
muhidtech <muhideen894@gmail.com>
2025-05-08 15:19:30 +0000

    MuhidTech

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,25 +1,26 @@
-"use client"
-import React from 'react'
-import { useRouter } from 'next/navigation'
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
 
+export default function Page() { // Renamed to "Page"
+  const route = useRouter();
+  const text = "Go Back";
 
-
-export default function page() {
-  const route = useRouter()
-  const text ="Go Back"
   return (
-    <div className='relative flex flex-col items-center justify-center h-screen w-full'>
-        <h1 className='flex flex-col items-center justify-center text-center'>
-            <span className='text-4xl font-bold text-gray-200'>Coming Soon</span>
-            <br />
-            <span className='text-lg text-gray-300'>This page is under construction</span>
-            <br />
-        </h1>
+    <div className="relative flex flex-col items-center justify-center h-screen w-full">
+      <h1 className="flex flex-col items-center justify-center text-center">
+        <span className="text-4xl font-bold text-gray-200">Coming Soon</span>
+        <br />
+        <span className="text-lg text-gray-300">This page is under construction</span>
+        <br />
+      </h1>
 
-        <button onClick={() => route.back()} className='cursor-pointer top-20 left-1/2 py-3 px-2 bg-cyan-500 rounded-lg'>
-          {text}
-        </button>
+      <button
+        onClick={() => route.back()}
+        className="cursor-pointer top-20 left-1/2 py-3 px-2 bg-cyan-500 rounded-lg"
+      >
+        {text}
+      </button>
     </div>
-  )
+  );
 }
-
