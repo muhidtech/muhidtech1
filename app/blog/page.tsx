3b127274d43cb055d3a8066bb2931821c0f80abe
muhidtech <muhideen894@gmail.com>
2025-05-08 15:05:56 +0000

    MMuhidTech

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,17 +1,5 @@
 import React from 'react'
-
-function pagee() {
-  return (
-    <div className='flex flex-col items-center justify-center h-screen bg-gray-100 w-full'>
-        <h1 className='flex flex-col items-center justify-center text-center'>
-            <span className='text-4xl font-bold text-gray-800'>Coming Soon</span>
-            <br />
-            <span className='text-lg text-gray-600'>This page is under construction</span>
-            <br />
-        </h1>
-    </div>
-  )
-}
+import { useRouter } from 'next/navigation'
 
 export default pagee
 export const metadata = {
@@ -20,3 +8,22 @@ export const metadata = {
     keywords: 'blog, coming soon',
   }
 export const dynamic = 'force-dynamic' // This will force the page to be dynamic
+
+function pagee() {
+  const route = useRouter()
+  const text = "< Go Back"
+  return (
+    <div className='relative flex flex-col items-center justify-center h-screen w-full'>
+        <h1 className='flex flex-col items-center justify-center text-center'>
+            <span className='text-4xl font-bold text-gray-800'>Coming Soon</span>
+            <br />
+            <span className='text-lg text-gray-600'>This page is under construction</span>
+            <br />
+        </h1>
+
+        <button className='absolute top-10 left-1/2 py-3 px-2 bg-cyan-500 rounded-lg'>
+          {text}
+        </button>
+    </div>
+  )
+}
